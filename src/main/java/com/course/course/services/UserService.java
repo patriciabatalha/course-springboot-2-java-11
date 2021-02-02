c5711c013d1a714a41c6819510d4176c943a6d4b
Patricia Batalha <patibatalha@gmail.com>
2021-02-02 20:51:00 -0300

    Exception handling - findById

--- src/main/java/com/course/course/services/UserService.java
+++ src/main/java/com/course/course/services/UserService.java
@@ -2,6 +2,7 @@ package com.course.course.services;
 
 import com.course.course.entities.User;
 import com.course.course.repositories.UserRepository;
+import com.course.course.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,24 +15,24 @@ public class UserService {
     @Autowired
     private UserRepository repository;
 
-    public List<User> findAll(){
+    public List<User> findAll() {
         return repository.findAll();
     }
 
-    public User findById(Long id){
+    public User findById(Long id) {
         Optional<User> obj = repository.findById(id);
-        return obj.get();
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
-    public User insert(User obj){
+    public User insert(User obj) {
         return repository.save(obj);
     }
 
-    public void delete(Long id){
+    public void delete(Long id) {
         repository.deleteById(id);
     }
 
-    public User update(Long id, User obj){
+    public User update(Long id, User obj) {
         User entity = repository.getOne(id);
         updateData(entity, obj);
         return repository.save(entity);
